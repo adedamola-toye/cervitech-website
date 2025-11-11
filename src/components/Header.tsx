0bac45753bcdbe8b8aae2c25e477cff3b224540f
adedamola-toye <toye.adedamola@gmail.com>
2025-11-11 15:40:02 +0100

    change background colour and add up arrow icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar"
 import "../styles/Navbar.css"
 import IphonePic from "../assets/iPhone 15 Pro 1.png"
 import "../styles/Header.css"
+import UpArrow from "../assets/lets-icons_up-light.png"
 export default function Header(){
     return(
         <>
@@ -12,7 +13,8 @@ export default function Header(){
                     <p>Cervitech helps you correct your neck posture while using your devices and provides posture correction exercises</p>
                     <button className="join-btn">Join waitlist</button>
                 </div>
-                <img src={IphonePic} alt="Iphone picture showing Cervitech" />
+                <img className="iphone-pic" src={IphonePic} alt="Iphone picture showing Cervitech" />
+                <img className="up-arrow" src={UpArrow} alt="Upward arrow"/>
             </div>
         </>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,12 @@ import HowCervitechHelps from "../components/HowCervitechHelps";
 
 function Home(){
     return(
-        <>
+        <div className="home">
             <Header/>
             <WhoIsCervitechFor/>
             <HowCervitechHelps/>
             <AppsPic/>
-        </>
+        </div>
     )
 }
 export default Home;
