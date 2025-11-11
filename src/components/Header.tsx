6150937b46035af5605b6f75f60303e2b7f8fb9c
adedamola-toye <toye.adedamola@gmail.com>
2025-11-11 16:21:38 +0100

    adjust up arrow to be fixed. hover effects on join button and nav section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,20 +2,21 @@ import Navbar from "./Navbar"
 import "../styles/Navbar.css"
 import IphonePic from "../assets/iPhone 15 Pro 1.png"
 import "../styles/Header.css"
-import UpArrow from "../assets/lets-icons_up-light.png"
+
 export default function Header(){
     return(
-        <>
+        <div id="header">
             <Navbar/>
-            <div className="header">
+            <div className="header" id="header">
                 <div className="left">
                     <h1>Monitor Your <span>Neck Posture</span> With Your Smart Phone</h1>
                     <p>Cervitech helps you correct your neck posture while using your devices and provides posture correction exercises</p>
+                    <div className="join-btn-wrapper">
+                    </div>
                     <button className="join-btn">Join waitlist</button>
                 </div>
                 <img className="iphone-pic" src={IphonePic} alt="Iphone picture showing Cervitech" />
-                <img className="up-arrow" src={UpArrow} alt="Upward arrow"/>
             </div>
-        </>
+        </div>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,22 @@ import Header from "../components/Header";
 import WhoIsCervitechFor from "../components/WhoIsCervitechFor";
 import AppsPic from "../components/AppsPic"
 import HowCervitechHelps from "../components/HowCervitechHelps";
-
+import UpArrow from "../assets/lets-icons_up-light.png"
+import "../styles/Home.css"
 function Home(){
+    const scrollToTop = () => {
+    const headerSection = document.getElementById("header");
+    if (headerSection) {
+      headerSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
     return(
         <div className="home">
             <Header/>
             <WhoIsCervitechFor/>
             <HowCervitechHelps/>
             <AppsPic/>
+            <img className="up-arrow" src={UpArrow} alt="Upward arrow" onClick={scrollToTop}/>
         </div>
     )
 }
