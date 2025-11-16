8470c53c4b5c787d7a4c86a49128b34bb8b83bcb
adedamola-toye <toye.adedamola@gmail.com>
2025-11-16 17:30:05 +0100

    footer completed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,43 @@
+import CervitechLogo from "../assets/Cervitech Logo White.png"
+//import { Link } from "react-router-dom";
+import FacebookIcon from "../assets/Socials Icons/bi_facebook.png";
+import TwitterIcon from "../assets/Socials Icons/akar-icons_twitter-fill.png";
+import InstagramIcon from "../assets/Socials Icons/ant-design_instagram-filled.png";
+import LinkedInIncon from "../assets/Socials Icons/akar-icons_linkedin-fill.png";
+import "../styles/Footer.css";
+function Footer() {
+  return (
+    <div className="footer-section">
+      <div className="footer-content">
+        <div className="left">
+          <img src={CervitechLogo} />
+          <p>
+            We develop and deliver cutting-edge digital technology to help you
+            prevent, correct, and manage abnormal posture and spinal pain. Our
+            focus is on empowering you to achieve optimal spinal well-being and
+            live a pain-free life.
+          </p>
+        </div>
 
-function Footer(){
-    return(
-        <>
-        </>
-    )
+        <div className="links">
+          <p className="link">FAQs</p>
+          <p className="link">Contact Us</p>
+          <p className="link">Privacy Policy</p>
+        </div>
+
+        <div className="socials">
+          <img src={TwitterIcon} alt="Twitter icon" />
+          <img src={InstagramIcon} alt="Instagram icon" />
+          <img src={FacebookIcon} alt="Facebook icon" />
+          <img src={LinkedInIncon} alt="Linked icon" />
+        </div>
+      </div>
+
+      <hr />
+
+      <footer>Copyright © 2024 Cervitech App. All rights reserved.</footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,7 @@ import UpArrow from "../assets/lets-icons_up-light.png"
 import "../styles/Home.css"
 import GetInTouch from "../components/GetInTouch";
 import LaunchCtaSection from "../components/LaunchCtaSection";
+import Footer from "../components/Footer";
 function Home(){
     const scrollToTop = () => {
     const headerSection = document.getElementById("header");
@@ -21,6 +22,7 @@ function Home(){
             {/* <AppsPic/> */}
             <GetInTouch/>
             <LaunchCtaSection/>
+            <Footer/>
             <img className="up-arrow" src={UpArrow} alt="Upward arrow" onClick={scrollToTop}/>
         </div>
     )
